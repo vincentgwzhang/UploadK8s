7e193bacc1e65d608251e399c3d9c284eb30df14
vincent.zhang <v-zhangv@iata.org>
2024-01-03 09:34:40 +0100

    project init

---
+++ src/main/java/org/example/upload/UploadK8sApplication.java
@@ -0,0 +1,13 @@
+package org.example.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadK8sApplication.class, args);
+	}
+
+}
